7dab4c83307761072e93d86b496c980de0619bdc
Vasudevan Govardhanen <48339921+p1npo1nt@users.noreply.github.com>
2024-11-21 17:41:44 -0500

    Update Main.java

--- linked-list/Main.java
+++ linked-list/Main.java
@@ -1,12 +1,46 @@
-// To run, compile *.java (javac *.java) and then run Main (java Main)
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         myList list = new myList();
-        list.insert("a");
-        list.insert("b");
-        list.insert("c");
 
-        list.print();
+        //* INPUT BLOCK
+        Scanner sc = new Scanner(System.in); 
+
+        while(true) {
+            System.out.print("Enter cmd: ");
+            // reads input and trims whitespaces, 'rest' variable contains the input excluding the command
+            String input = sc.nextLine().trim();
+            String rest = input.substring(1, input.length()).trim();
+
+            if (input.isEmpty()) {
+                throw new IllegalArgumentException("No input command provided");
+            }
+
+            switch(input.charAt(0)) {
+                case 'p':
+                    list.print();
+                    break;
+                case 'i':
+                    list.insert(rest);
+                    System.out.println("Inserted: " + rest);
+                    break;
+                case 'd':
+                    if (list.delete(rest)) {
+                        list.delete(rest); 
+                        System.out.println("Deleted: " + rest);
+                    } else {
+                        System.out.println("Could not delete: " + rest);
+                    }
+                    break;
+                case 'c':
+                    System.out.println(list.contains(rest));
+                    break;
+                case 'q':
+                    sc.close();
+                    System.out.println("Exiting...");
+                    return;
+            }
+        }
     }
 }
